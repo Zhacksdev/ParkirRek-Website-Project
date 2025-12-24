98a2a74e8521949f7820b5ca37d018853831bddd
adithyaananda2005-coder <adithyaananda2005@gmail.com>
2025-12-24 17:09:01 +0700

    add: seeder database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,24 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// Hapus use App\Models\User; karena kita pakai seeder manual
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // Panggil seeder urut dari induk ke anak (PENTING URUTANNYA)
+        $this->call([
+            UserSeeder::class,          // 1. Buat User & Admin
+            AdminSeeder::class,         // 2. Masukkan ke tabel Admin
+            KendaraanSeeder::class,     // 3. Beri kendaraan ke User
+            ParkingRecordSeeder::class, // 4. Buat history parkir
+            LaporanParkirSeeder::class, // 5. Buat laporan
+            PelanggaranSeeder::class,   // 6. Buat data pelanggaran
         ]);
     }
 }
